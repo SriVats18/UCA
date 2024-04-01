3ccc71119424c40875cedb11907cbce18170d8f9
SriVats18 <vatsnagwan0318@gmail.com>
2024-04-01 19:35:24 +0530

    added paragraph

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
         <div className='h-lvh'>
             <div className='w-full bg-slate-500 '><ul className='flex justify-between'><li>home</li><li>about</li><li>contact</li></ul></div>
             <h1>hello world</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
         </div>
     )
 }
