5a0df98845e2d04e45cc0e76b451ba24d1f1977d
Vikash2048 <rpsvikash16@gmail.com>
2024-04-01 19:28:46 +0530

    file created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Content from "./Pages/Content"
+import Home from "./Pages/Home"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Home/>
+      <Content/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='h-lvh'>
+            <div className='w-full bg-slate-500 '><ul className='flex justify-between'><li>home</li><li>about</li><li>contact</li></ul></div>
+            <h1>hello world</h1>
+        </div>
+    )
+}
+
+export default Home
